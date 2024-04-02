bf8b09ea4956f38dfc0fd70fe1726ac7d666dc70
Ewerton Nascimento <ewertonvinicius@outlook.com.br>
2024-04-02 19:53:51 -0300

    correção de bugs do dark mode

--- js/light-dark-mode-change.js
+++ js/light-dark-mode-change.js
@@ -9,25 +9,51 @@ const modeTogglerContainer2light = document.querySelector('.container-2');
 const modeTogglerContainer3light = document.querySelector('.container-3');
 const modeTogglerContainer4light = document.querySelector('.container-4');
 
+
 modeTogglerDark.addEventListener('click', () => {
   document.body.classList.remove('light-mode');
   document.body.classList.add('dark-mode');
+  modeTogglerContainer1light.classList.remove('container-1');
+  modeTogglerContainer2light.classList.remove('container-2');
+  modeTogglerContainer3light.classList.remove('container-3');
+  modeTogglerContainer4light.classList.remove('container-4');
+  modeTogglerContainer1light.classList.add('containerDark-1');
+  modeTogglerContainer2light.classList.add('containerDark-2');
+  modeTogglerContainer3light.classList.add('containerDark-3');
+  modeTogglerContainer4light.classList.add('containerDark-4');
 });
 
 modeTogglerLight.addEventListener('click', () => {
   document.body.classList.remove('dark-mode');
   document.body.classList.add('light-mode');
+  modeTogglerContainer1light.classList.remove('containerDark-1');
+  modeTogglerContainer2light.classList.remove('containerDark-2');
+  modeTogglerContainer3light.classList.remove('containerDark-3');
+  modeTogglerContainer4light.classList.remove('containerDark-4');
+  modeTogglerContainer1light.classList.add('container-1');
+  modeTogglerContainer2light.classList.add('container-2');
+  modeTogglerContainer3light.classList.add('container-3');
+  modeTogglerContainer4light.classList.add('container-4');
 });
+// modeTogglerDark.addEventListener('click', () => {
+//   document.body.classList.remove('light-mode');
+//   document.body.classList.add('dark-mode');
+// });
 
-function addClickEventToDarkContainers(container, darkClass) {
-  container.addEventListener('click', () => {
-    container.classList.remove('container-1', 'container-2', 'container-3', 'container-4');
-    container.classList.add(darkClass);
-  });
-}
+// modeTogglerLight.addEventListener('click', () => {
+//   document.body.classList.remove('dark-mode');
+//   document.body.classList.add('light-mode');
+// });
+
+// function addClickEventToDarkContainers(container, darkClass) {
+//   container.addEventListener('click', () => {
+//     container.classList.remove('container-1', 'container-2', 'container-3', 'container-4');
+//     container.classList.add(darkClass);
+//   });
+// }
 
 
-addClickEventToDarkContainers(modeTogglerContainer1light, 'containerDark-1');
-addClickEventToDarkContainers(modeTogglerContainer2light, 'containerDark-2');
-addClickEventToDarkContainers(modeTogglerContainer3light, 'containerDark-3');
-addClickEventToDarkContainers(modeTogglerContainer4light, 'containerDark-4');
+// addClickEventToDarkContainers(modeTogglerContainer1light, 'containerDark-1');
+// addClickEventToDarkContainers(modeTogglerContainer2light, 'containerDark-2');
+// addClickEventToDarkContainers(modeTogglerContainer3light, 'containerDark-3');
+// addClickEventToDarkContainers(modeTogglerContainer4light, 'containerDark-4');
